32a54168332b2a6b8b7542a78d8b8971d42c9e01
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-03-24 15:09:09 -0400

    added object filterig

---
+++ fillteringObjects.js
@@ -0,0 +1,34 @@
+// let person1 = {
+//     name: 'John',
+//      birthdayInfo: {
+//       bday: 'jun',
+//       age:29
+//     },
+//     accountAge: 200
+//   }
+
+//   let person2 = {
+//     name: 'Jane',
+//     birthdayInfo: {
+//       bday: 'mar',
+//       age:33
+//     },
+//     accountAge: 0
+//   }
+
+//   let person3 = {
+//     name: 'Carl',
+//     birthdayInfo: {
+//       bday: 'dec',
+//       age:19
+//     },
+//     accountAge: 7
+//   }
+
+//   let personArr = [person1,person2, person3];
+
+//   let ageOrder = [...personArr].sort(function(a,b){
+//     return b.birthdayInfo.age - a.birthdayInfo.age
+//   })
+
+//   console.log(ageOrder)
